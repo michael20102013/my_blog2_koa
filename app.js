5b8f3d3151afe9e51b07fdae34f9a2237b1f9ad1
iwangcx <3978739275@qq.com>
2019-05-11 22:26:01 +0800

    增大文件上传大小 Signed-off-by: iwangcx <3978739275@qq.com>

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app
     .use(koaBody({
         multipart: true,
         formidable: {
-            maxFileSize: 2000 * 1024 * 1024    // 设置上传文件大小最大限制，默认2M
+            maxFileSize: 20000 * 1024 * 1024    // 设置上传文件大小最大限制，默认2M
         }
     }))
     // .use(cors({
